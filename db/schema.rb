2470d8666804ae5ac8ae89c898ca63cd7831853d
wata-ryu <71304058ryu@gmail.com>
2023-03-09 13:29:22 +0000

    SNACKERS:3/9-2

--- db/schema.rb
+++ db/schema.rb
@@ -46,9 +46,9 @@ ActiveRecord::Schema.define(version: 2023_03_08_043057) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "is_deleted"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
