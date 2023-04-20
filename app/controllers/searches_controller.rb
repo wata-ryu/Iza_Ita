fe2c7cd15a702f2c215dff98378ac14e455f9300
wata-ryu <71304058ryu@gmail.com>
2023-04-20 07:49:17 +0000

    ロゴ修正:4/20

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,17 +1,17 @@
 class SearchesController < ApplicationController
-
+  #user,admin共通検索機能
   def search
     @range = params[:range]
     @word = params[:word]
 
     if @range == "User"
-      #検索対象モデルがUserの時、admin用
-      @users_admin = User.looks_for_admin(params[:search], params[:word]).page(params[:page])
-      #検索対象モデルがUserの時、user用
-      @users = User.looks(params[:search], params[:word]).page(params[:page])
+      #検索対象モデルがUserの時、admin用（退会ユーザー含む全て表示 + 最新を上に表示 + kaminariでのページネーション）
+      @users_admin = User.looks_for_admin(params[:search], params[:word]).order("created_at DESC").page(params[:page])
+      #検索対象モデルがUserの時、user用（退会ユーザーは表示しない + 最新を上に表示 + kaminariでのページネーション）
+      @users = User.looks(params[:search], params[:word]).order("created_at DESC").page(params[:page])
     else
-      #タイトルの時
-      @posts = Post.looks(params[:search], params[:word]).page(params[:page])
+      #タイトルの時（公開設定した投稿のみ表示 + 最新を上に表示 + kaminariでのページネーション）
+      @posts = Post.released.looks(params[:search], params[:word]).order("created_at DESC").page(params[:page])
     end
   end
 end
